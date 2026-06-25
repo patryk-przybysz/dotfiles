e960546a143ec0470d81fbb956d7dad5ec9b4975
Patryk Przybysz <pprzybysz04@outlook.com>
2026-06-25 13:16:39 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Home Manager configuration of patryk";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      homeConfigurations."patryk" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [ ./home.nix ];
+
+        extraSpecialArgs = { inherit inputs system; };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,83 @@
+{
+  pkgs,
+  config,
+  inputs,
+  system,
+  ...
+}:
+{
+  home.username = "patryk";
+  home.homeDirectory = "/home/patryk";
+
+  home.stateVersion = "25.05";
+
+  home.packages = with pkgs; [
+    docker-language-server
+    ormolu
+
+    devcontainer
+    typst
+    fastfetch
+    rustup
+    devenv
+    oci-cli
+    terraform
+
+    # C/C++
+    gcc
+    cmake
+    gnumake
+
+    # Nix
+    nil
+    nixfmt
+
+    # Fonts
+    nerd-fonts.commit-mono
+    libertine
+    font-awesome
+    corefonts
+    vista-fonts
+  ];
+
+  fonts.fontconfig.enable = true;
+
+  programs.bun.enable = true;
+  programs.uv.enable = true;
+
+  programs.fd.enable = true;
+  programs.htop.enable = true;
+  programs.jq.enable = true;
+  programs.zellij.enable = true;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.gh = {
+    enable = true;
+  };
+
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      extra-experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      auto-optimise-store = true;
+    };
+  };
+  programs.nh = {
+    enable = true;
+    flake = "${config.home.homeDirectory}/.config/home-manager";
+    clean = {
+      enable = true;
+      dates = "weekly";
+      extraArgs = "--keep 5 --keep-since 14d";
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
